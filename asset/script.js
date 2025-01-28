8d55748cefb4e41b48ca91ae99657dc6c776872d
Muhammad Mohsin <mmm29314782@gmail.com>
2025-01-29 03:21:20 +0500

    responsive working

---
+++ asset/script.js
@@ -0,0 +1,9 @@
+  // Show the second navbar on scroll
+  window.addEventListener('scroll', () => {
+    const secondNavbar = document.getElementById('second-navbar');
+    if (window.scrollY > 100) { // Adjust scroll value as needed
+      secondNavbar.style.display = 'block';
+    } else {
+      secondNavbar.style.display = 'none';
+    }
+  });
