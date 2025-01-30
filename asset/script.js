ffa5c018e3102fef36cc5c8915ba571b8e350ad5
Muhammad Mohsin <mmm29314782@gmail.com>
2025-01-30 06:10:52 +0500

    reviews pending

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,7 @@
   // Show the second navbar on scroll
   window.addEventListener('scroll', () => {
     const secondNavbar = document.getElementById('second-navbar');
-    if (window.scrollY > 100) { // Adjust scroll value as needed
+    if (window.scrollY > 150) { // Adjust scroll value as needed
       secondNavbar.style.display = 'block';
     } else {
       secondNavbar.style.display = 'none';
